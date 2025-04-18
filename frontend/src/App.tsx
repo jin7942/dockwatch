3187393ab781b3c2dbcbd25a5d18dc3de865bceb
jin7742 <infomer.jin@gmail.com>
2025-04-19 00:57:07 +0900

    init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
-import './App.css';
+import { useState } from 'react';
 
 function App() {
-    return <div className='bg-blue-500 text-white p-4 rounded-md'>TailwindCSS 정상 작동 중!</div>;
+    return <></>;
 }
 
 export default App;
