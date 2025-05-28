c66d9c48077199cb1450093ab94e28e662b12a72
jin7742 <infomer.jin@gmail.com>
2025-05-29 01:19:48 +0900

    docs: Update constnats

--- frontend/src/common/_config/constants.ts
+++ frontend/src/common/_config/constants.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
-    API_BASE_URL: 'http://dockwatch-backend:3738/api',
-    WS_BASE_URL: 'ws://dockwatch-backend:3738/ws',
+    API_BASE_URL: 'https://dockwatch.jin7942.co.kr/api',
+    WS_BASE_URL: 'wss://dockwatch.jin7942.co.kr/ws',
 };
