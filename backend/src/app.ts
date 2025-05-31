92fc2a501972c3484180067e7897d119e374f110
jin7742 <infomer.jin@gmail.com>
2025-05-31 22:46:07 +0900

    fix(backend): Change cors config for websocket

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,7 +11,11 @@ import { CONFIG } from './common/_config/constants';
 const app = express();
 
 // cors 설정
-app.use(
+app.use((req, res, next) => {
+    if (req.headers.upgrade && req.headers.upgrade.toLowerCase() === 'websocket') {
+        // WebSocket Upgrade 요청은 CORS 검사 건너뜀
+        return next();
+    }
     cors({
         origin: (origin, callback) => {
             if (!origin || CONFIG.ORIGIN_URL.includes(origin)) {
@@ -20,9 +24,9 @@ app.use(
                 callback(new Error('CORS 차단: 허용되지 않은 Origin'));
             }
         },
-        credentials: false, // 쿠키나 세션이 필요한 경우
-    }),
-);
+        credentials: false,
+    })(req, res, next);
+});
 
 // 기본 미들웨어 설정
 app.use(express.json());
